7ca465674ae1c066e2d93d2b149e965da71ab9ba
Володя <72095217+VolodjaViv@users.noreply.github.com>
2021-11-17 19:39:41 +0300

    Add files via upload

---
+++ RiscV_processor_simulator/cmake-build-debug/CMakeFiles/riscv_sim.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Vahmurka/Desktop/RiscV_processor_simulator/5_Ghovin_2/RiscV_processor_simulator/src/Instruction.cpp" "C:/Users/Vahmurka/Desktop/RiscV_processor_simulator/5_Ghovin_2/RiscV_processor_simulator/cmake-build-debug/CMakeFiles/riscv_sim.dir/src/Instruction.cpp.obj"
+  "C:/Users/Vahmurka/Desktop/RiscV_processor_simulator/5_Ghovin_2/RiscV_processor_simulator/src/main.cpp" "C:/Users/Vahmurka/Desktop/RiscV_processor_simulator/5_Ghovin_2/RiscV_processor_simulator/cmake-build-debug/CMakeFiles/riscv_sim.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RiscV_processor_simulator/cmake-build-debug/CMakeFiles/riscv_sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/riscv_sim.dir/src/Instruction.cpp.obj"
+  "CMakeFiles/riscv_sim.dir/src/main.cpp.obj"
+  "riscv_sim.exe"
+  "riscv_sim.exe.manifest"
+  "riscv_sim.lib"
+  "riscv_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/riscv_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
